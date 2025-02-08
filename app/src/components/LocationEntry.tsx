5adf7ac06c018b3b967efd1339189943e0f54734
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2025-02-08 18:57:13 +0100

    Enhance LocationEntry form validation; add error handling for title, identifier, number, and address fields, and update layout for better usability

--- app/src/components/LocationEntry.tsx
+++ app/src/components/LocationEntry.tsx
@@ -30,60 +30,60 @@ function LocationView({ location, onEdit }: any) {
     );
 }
 
-function LocationForm({ location, setLocation, onCancel, onSave, onDelete }: any) {
+function LocationForm({ location, error, setLocation, onCancel, onSave, onDelete }: any) {
     return (
         <div className="locationEntry-editing">
             <div className="locationEntry-info-editing">
 
                 <div />
-                <Input.Wrapper label="Titel" description="" error="">
-                    <Input
-                        radius="xs"
-                        variant="default"
-                        placeholder="Titel"
-                        value={location.title}
-                        onChange={(e) => setLocation((prev: any) => ({ ...prev, title: e.target.value }))}
-                    />
-                </Input.Wrapper>
+                <TextInput
+                    label="Titel"
+                    error={error.title}
+                    radius="xs"
+                    variant="default"
+                    placeholder="Titel"
+                    value={location.title}
+                    onChange={(e) => setLocation((prev: any) => ({ ...prev, title: e.target.value }))}
+                />
                 <div />
 
                 <div />
-                <Input.Wrapper label="Kurzbezeichnung" description="" error="">
-                    <TextInput
-                        radius="xs"
-                        variant="default"
-                        placeholder="Kurzbezeichnung"
-                        value={location.identifier}
-                        onChange={(e) => setLocation((prev: any) => ({ ...prev, identifier: e.target.value }))}
-                    />
-                </Input.Wrapper>
-                <Input.Wrapper label="Kennnummer" description="" error="">
-                    <TextInput
-                        radius="xs"
-                        variant="default"
-                        placeholder="Kennnummer"
-                        value={location.number}
-                        onChange={(e) => setLocation((prev: any) => ({ ...prev, number: e.target.value }))}
-                    />
-                </Input.Wrapper>
+                <TextInput
+                    label="Kurzbezeichnung"
+                    error={error.identifier}
+                    radius="xs"
+                    variant="default"
+                    placeholder="Kurzbezeichnung"
+                    value={location.identifier}
+                    onChange={(e) => setLocation((prev: any) => ({ ...prev, identifier: e.target.value }))}
+                />
+                <TextInput
+                    label="Kennnummer"
+                    error={error.number}
+                    radius="xs"
+                    variant="default"
+                    placeholder="Kennnummer"
+                    value={location.number}
+                    onChange={(e) => setLocation((prev: any) => ({ ...prev, number: e.target.value }))}
+                />
 
 
                 <div />
-                <Input.Wrapper label="Adresse" description="" error="">
-                    <TextInput
-                        radius="xs"
-                        variant="default"
-                        placeholder="Adresse"
-                        value={location.address}
-                        onChange={(e) => setLocation((prev: any) => ({ ...prev, address: e.target.value }))}
-                    />
-                </Input.Wrapper>
+                <TextInput
+                    label="Adresse"
+                    error={error.address}
+                    radius="xs"
+                    variant="default"
+                    placeholder="Adresse"
+                    value={location.address}
+                    onChange={(e) => setLocation((prev: any) => ({ ...prev, address: e.target.value }))}
+                />
                 <div />
             </div>
             <div className="locationEntry-actions">
                 <Button variant="default" color="red" onClick={onCancel}>Abbrechen</Button>
                 <Button color="green" onClick={onSave}>Speichern</Button>
-                <MdDelete className="delete-icon" color="grey" size={23} />
+                <MdDelete className="delete-icon" color="grey" size={23} onClick={onDelete} style={{ cursor: "pointer" }} />
             </div>
         </div>
     );
@@ -98,6 +98,13 @@ export default function LocationEntry() {
         address: 'Hüttenstraße 68',
     });
 
+    const [error, setError] = useState({
+        title: '',
+        identifier: '',
+        number: '',
+        address: '',
+    });
+
     const [editLocation, setEditLocation] = useState({
         title: location.title,
         identifier: location.identifier,
@@ -107,17 +114,45 @@ export default function LocationEntry() {
 
     const [isEditing, setIsEditing] = useState<boolean>(false);
 
+    function resetError() {
+        setError({
+            title: '',
+            identifier: '',
+            number: '',
+            address: '',
+        });
+    }
+
     function handleSave() {
+        resetError();
+        if (!editLocation.title) {
+            setError((prev: any) => ({ ...prev, title: 'Titel darf nicht leer sein' }));
+            return;
+        }
+        if (!editLocation.identifier) {
+            setError((prev: any) => ({ ...prev, identifier: 'Kurzbezeichnung darf nicht leer sein' }));
+            return;
+        }
+        if (!editLocation.number) {
+            setError((prev: any) => ({ ...prev, number: 'Kennnummer darf nicht leer sein' }));
+            return;
+        }
+        if (!editLocation.address) {
+            setError((prev: any) => ({ ...prev, address: 'Adresse darf nicht leer sein' }));
+            return;
+        }
         setLocation(editLocation);
         setIsEditing(false);
     }
 
     function handleCancel() {
+        resetError();
         setEditLocation(location);
         setIsEditing(false);
     }
 
     function handleDelete() {
+        resetError();
         setIsEditing(false);
     }
 
@@ -125,6 +160,7 @@ export default function LocationEntry() {
         <div className="locationEntry-content">
             {isEditing ? (
                 <LocationForm
+                    error={error}
                     location={editLocation}
                     setLocation={setEditLocation}
                     onCancel={() => handleCancel()}
