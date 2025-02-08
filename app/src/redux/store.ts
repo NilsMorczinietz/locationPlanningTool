353295bfeea40e0313c90408e1486d07bbd10558
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2025-02-08 21:46:29 +0100

    Refactor Redux store configuration to remove unnecessary comments and ensure proper type usage for persisted reducer

--- app/src/redux/store.ts
+++ app/src/redux/store.ts
@@ -3,7 +3,6 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { locationsReducer } from './reducers/locationsReducer';
 
-// Hier den Typ für den State definieren
 import type { LocationsState } from './reducers/locationsReducer';
 
 const persistConfig = {
@@ -11,12 +10,11 @@ const persistConfig = {
     storage,
 };
 
-// Typisierung für den persistierten Reducer
 const persistedReducer = persistReducer<LocationsState>(persistConfig, locationsReducer);
 
 export const store = configureStore({
     reducer: {
-        planning: persistedReducer, // Jetzt mit richtigem Typ
+        planning: persistedReducer,
     },
     devTools: true,
     middleware: (getDefaultMiddleware) =>
