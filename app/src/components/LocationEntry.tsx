d750d5bdc08f31eff1f7e47d8a7e51b3257bc0b1
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2025-02-08 13:06:33 +0100

    Implement editable fields in LocationEntry; add state management for title, identifier, and address

--- app/src/components/LocationEntry.tsx
+++ app/src/components/LocationEntry.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import './LocationEntry.css';
 import { Checkbox } from '@mantine/core';
 import { HiDotsVertical } from "react-icons/hi";
+import { TextInput } from '@mantine/core';
+import { Button } from '@mantine/core';
+import { Input } from '@mantine/core';
 
 export default function LocationEntry() {
 
@@ -10,29 +13,84 @@ export default function LocationEntry() {
     const [number, setNumber] = useState<string>('Nr.: L');
     const [address, setAddress] = useState<string>('Hüttenstraße 68');
 
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+
     return (
         <>
             <div className="locationEntry-content">
-                <div className="locationEntry-info">
-                    <div>
-                        <Checkbox
-                            defaultChecked={true}
-                            color="red"
-                        />
+                {isEditing ? (
+                    <div style={{display:"flex", flexDirection:"column"}}>
+                        <div className="locationEntry-info">
+                            <div>
+                                <Checkbox
+                                    defaultChecked={true}
+                                    color="red"
+                                />
+                            </div>
+                            <div style={{display:"flex", width:"100%"}}>
+                                <Input
+                                    placeholder='Titel'
+                                    value={title}
+                                    onChange={(event) => setTitle(event.currentTarget.value)}
+                                />
+                            </div>
+                            <div></div>
+
+                            <div></div>
+                            <div>
+                                <TextInput
+                                    placeholder='Kurzbezeichnung'
+                                    value={identifier}
+                                    onChange={(event) => setIdentifier(event.currentTarget.value)}
+                                />
+                            </div>
+                            <div>
+                                <TextInput
+                                    placeholder='Kennnummer'
+                                    value={number}
+                                    onChange={(event) => setNumber(event.currentTarget.value)}
+                                />
+                            </div>
+
+                            <div></div>
+                            <div>
+                                <TextInput
+                                    placeholder='Addresse'
+                                    value={address}
+                                    onChange={(event) => setAddress(event.currentTarget.value)}
+                                />
+                            </div>
+                            <div></div>
+                        </div>
+                        {/* <div style={{display:"flex", justifyContent:"space-between", flexDirection:"row", gap:"10px", padding:"10px"}}>
+                            <Button fullWidth color="red">Abbrechen</Button>
+                            <Button fullWidth color="green">Speichern</Button>
+                        </div> */}
                     </div>
-                    <div> {title} </div>
-                    <div></div>
+                ) : (
+                    <>
+                        <div className="locationEntry-info">
+                            <div>
+                                <Checkbox
+                                    defaultChecked={true}
+                                    color="red"
+                                />
+                            </div>
+                            <div>{title}</div>
+                            <div></div>
 
-                    <div></div>
-                    <div> {identifier} </div>
-                    <div> {number} </div>
+                            <div></div>
+                            <div> {identifier} </div>
+                            <div> {number} </div>
 
-                    <div></div>
-                    <div> {address} </div>
-                    <div></div>
-                </div>
-                <div className="locationEntry-edit">
-                    <HiDotsVertical size={20} /> 
+                            <div></div>
+                            <div> {address} </div>
+                            <div></div>
+                        </div>
+                    </>
+                )}
+                <div className="locationEntry-edit" onClick={() => setIsEditing(true)}>
+                    <HiDotsVertical size={20} style={{ cursor: "pointer" }} />
                 </div>
             </div>
         </>
