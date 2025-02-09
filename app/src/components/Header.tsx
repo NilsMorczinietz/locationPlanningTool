37640af82689bb532509a96855c233a8d6c14cc8
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2025-02-10 00:15:39 +0100

    Add upload and download buttons to Header component

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,9 +1,48 @@
+import { Button } from '@mantine/core';
+
+import { FiDownload } from "react-icons/fi";
+import { FiUpload } from "react-icons/fi";
+
 import fw_dus_logo from '/fw_dus_logo.png';
 
 export default function Header() {
+
+    function download() {
+        //TODO: Implement download
+    }
+
+    function upload() {
+        //TODO: Implement upload
+    }
+
     return (
-        <header style={{display:"flex", alignItems:"center", padding:"5px"}}>
-            <img src={fw_dus_logo} alt="Logo" width="auto" height="90%" />
+        <header style={{ display: "flex", alignItems: "center", padding: "5px", width: "100%", gap: "10px"}}>
+            <img
+                src={fw_dus_logo}
+                alt="Logo"
+                width="auto"
+                height="90%"
+            />
+
+            <div style={{ display: "flex", flex: "1" }}></div>
+
+            <Button
+                color="rgb(255, 255, 255)"
+                variant="outline"
+                rightSection={<FiUpload size={16} />}
+                onClick={download}
+            >
+                Upload
+            </Button>
+
+            <Button
+                color="rgb(255, 255, 255)"
+                variant="outline"
+                rightSection={<FiDownload size={16} />}
+                onClick={download}
+            >
+                Download
+            </Button>
         </header>
     );
 }
