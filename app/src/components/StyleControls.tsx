34cb7f51799f70ffa6e22c6c8961e548be82f776
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2025-02-09 18:28:12 +0100

    Implement toggle functionality in StyleControls for dynamic background color changes

--- app/src/components/StyleControls.tsx
+++ app/src/components/StyleControls.tsx
@@ -4,25 +4,30 @@ import {
     defaultVariantColorsResolver,
     VariantColorsResolver,
 } from '@mantine/core';
+import { useState } from 'react';
 
 import { MdLayers } from "react-icons/md";
 
-const variantColorResolver: VariantColorsResolver = (input) => {
-    const defaultResolvedColors = defaultVariantColorsResolver(input);
-
-    if (input.variant === 'control') {
-        return {
-            background: 'rgb(8, 8, 8)',
-            hover: 'rgb(0, 0, 0)',
-            color: 'none',
-            border: 'none',
-        };
-    }
-
-    return defaultResolvedColors;
-};
-
 export default function StyleControls() {
+
+    const [open, setOpen] = useState(false);
+
+
+    const variantColorResolver: VariantColorsResolver = (input) => {
+        const defaultResolvedColors = defaultVariantColorsResolver(input);
+    
+        if (input.variant === 'control') {
+            return {
+                background: open ? 'rgb(209, 42, 42)' : 'rgb(8, 8, 8)',
+                hover: open ? 'rgb(201, 34, 34)' : 'rgb(0, 0, 0)',
+                color: 'none',
+                border: 'none',
+            };
+        }
+    
+        return defaultResolvedColors;
+    };
+
     return (
         <div style={{ position: "absolute", top: "50px", right: "0", margin: "30px" }}>
             <MantineProvider theme={{ variantColorResolver }}>
@@ -32,6 +37,7 @@ export default function StyleControls() {
                     radius="xs"
                     aria-label="MapStyle"
                     style={{ border: "2px solid rgb(231, 231, 231)", borderRadius: "3px", boxShadow: "0px 0px 10px 1px rgb(134, 134, 134)" }}
+                    onClick={() => setOpen(!open)}
                 >
                     <MdLayers color='white' size={25} />
                 </ActionIcon>
